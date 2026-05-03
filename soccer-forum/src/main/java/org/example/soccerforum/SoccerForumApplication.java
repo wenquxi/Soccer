8e32305acf1c765fd28d8891cb7ab29bc5a3de77
文渠星 <16521147+wenqu-star@user.noreply.gitee.com>
2026-05-03 17:14:21 +0800

    项目初步设计

---
+++ soccer-forum/src/main/java/org/example/soccerforum/SoccerForumApplication.java
@@ -0,0 +1,13 @@
+package org.example.soccerforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerForumApplication.class, args);
+    }
+
+}
---
+++ soccer-forum/src/test/java/org/example/soccerforum/SoccerForumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.soccerforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
